6a5b2c3e20f9cde13c9d02aaf0f0331ab3798806
Arbaj-Ansari <arbajansari1818@gmail.com>
2024-12-11 20:44:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import Mobile from './mobile'
+
+
+function App() {
+  return (
+    <>
+
+    <Mobile
+
+    src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70"
+    name="Apple iPhone 15 (Black, 128 GB)"
+    price="₹58100"
+
+    />
+
+
+    <Mobile
+    src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/n/l/u/-original-imah2fjd7wfd9ksh.jpeg?q=70"
+    name="Motorola G85 5G (Olive Green, 128 GB)  (8 GB RAM)"
+    price="₹17,999"
+    />
+
+
+    <Mobile
+    src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/6/t/4/-original-imah3chxfkqxyzm3.jpeg?q=70"
+    name="realme 12x 5G (Woodland Green, 128 GB)  (8 GB RAM)"
+    price="₹13,499"
+    />
+
+    <Mobile
+       src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/z/l/z9x-i2219-iqoo-original-imahfa9hsrtkvgsd.jpeg?q=70"
+       name="IQOO z9x (Tornado Green, 128 GB)  (6 GB RAM)"
+       price="₹13,840"
+    />
+    <Mobile
+    src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/d/y/m/pixel-9-ga05843-in-google-original-imah3pfgd9zadkyx.jpeg?q=70"
+    name="Google Pixel 9 (Porcelain, 256 GB)  (12 GB RAM)"
+    price="₹79,999"
+    />
+    <Mobile
+    src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/g/p/d/nord-ce-3-lite-5g-cph2467-oneplus-original-imah4ehbgmz4qzhz.jpeg?q=70"
+    name="OnePlus Nord CE 3 Lite 5G (Pastel Lime, 128 GB)  (8 GB RAM)"
+    price="₹14,510"
+    />
+    
+
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/mobile.js
@@ -0,0 +1,50 @@
+
+import './mobile.css'
+
+function Mobile(props){
+    return(
+        <>
+
+        <div className="container">
+
+        <div id="image">
+            <img src={props.src}/>
+
+        </div>
+
+        <div id="details">
+            <h3>{props.name}</h3>
+
+            <ul>
+                        <li>128 GB ROM</li>
+                        <li>15.49 cm (6.1 inch) Super Retina XDR Display</li>
+                        <li>48MP + 12MP | 12MP Front Camera</li>
+                        <li>A16 Bionic Chip, 6 Core Processor Processor</li>
+            </ul>
+
+        </div>
+
+        <di id="price">
+            <h1>{props.price}</h1>
+
+            <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_9e47c1.png" width="150"/>
+
+        </di>
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+        </>
+    );
+}
+export default Mobile;
